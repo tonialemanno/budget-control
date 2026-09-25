96fb51ec03920b631b0b9963afc63ecd0c88cedb
tonialemanno <toni.alemanno@me.com>
2026-09-25 14:19:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,7 @@
-const CACHE = 'budget-control-v65-4-admin-modules-page-1';
+const CACHE = 'budget-control-v66-1-user-first-complete-2';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(async cache => {
-        for (const url of APP_SHELL) {
-          const res = await fetch(url, { cache: 'reload' });
-          if (res.ok) await cache.put(url, res.clone());
-        }
-      })
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
@@ -21,13 +12,8 @@ self.addEventListener('fetch', event => {
   const url = new URL(req.url);
   if (url.origin !== self.location.origin) return;
   if (req.mode === 'navigate') {
-    event.respondWith(
-      fetch(req, { cache: 'no-store' })
-        .then(res => { const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return res; })
-        .catch(()=>caches.match('./index.html'))
-    );
+    event.respondWith(fetch(req,{cache:'no-store'}).then(res => { const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return res; }).catch(()=>caches.match('./index.html')));
     return;
   }
-  if (url.pathname.endsWith('/sw.js')) return;
-  event.respondWith(caches.match(req).then(hit => hit || fetch(req, { cache: 'no-cache' }).then(res => { if (res.ok) caches.open(CACHE).then(c=>c.put(req,res.clone())); return res; })));
+  event.respondWith(caches.match(req).then(hit => hit || fetch(req).then(res => { if(res.ok) caches.open(CACHE).then(c=>c.put(req,res.clone())); return res; })));
 });
