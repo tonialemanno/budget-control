36f4bee7e1d822f85ad7827ff37747be8fe15b93
tonialemanno <toni.alemanno@me.com>
2026-09-25 09:19:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-control-v61-batch-1';
+const CACHE = 'budget-control-v62-year-import-1';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
