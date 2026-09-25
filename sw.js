882a774f1ff3ebcdcede0dac8df7d396043ecb94
tonialemanno <toni.alemanno@me.com>
2026-09-25 23:16:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'alo-agazone-v67-3-1-verified';
+const CACHE = 'alo-agazone-v67-4-0-verified';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
