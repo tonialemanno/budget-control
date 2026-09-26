ffb44164f96c24ead385262dad007cdba55f6b4f
tonialemanno <toni.alemanno@me.com>
2026-09-26 19:37:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aione-v68-0-2-interaction-fix';
+const CACHE = 'aione-v68-0-3-mfa-flow-fix';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
